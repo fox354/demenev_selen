869738f013c0341e99989f67fe19e4cec449ff41
Andrew Demenev <fox354@gmail.com>
2016-12-19 02:06:44 +0500

    Переписал на более понятный мне вариант

--- google_search_test.js
+++ google_search_test.js
@@ -1,36 +1,33 @@
 /**
- * @fileoverview An example test that may be run using Mocha.
- *
- * Usage:
- *
- *     mocha -t 10000 selenium-webdriver/example/google_search_test.js
- *
- * You can change which browser is started with the SELENIUM_BROWSER environment
- * variable:
- *
- *     SELENIUM_BROWSER=chrome \
- *         mocha -t 10000 selenium-webdriver/example/google_search_test.js
- */
+    chrome_driver = new webdriver.Builder().forBrowser('chrome').build();
+    ie_driver = new webdriver.Builder().forBrowser('ie').build();
+    firefox_driver = new webdriver.Builder().forBrowser('firefox').build();
+**/
 
-const {Builder, By, until} = require('selenium-webdriver');
-const test = require('selenium-webdriver/testing');
+var webdriver = require('selenium-webdriver'),
+    chrome = require('selenium-webdriver/chrome'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
 
 test.describe('Google Search', function() {
-  let driver;
+  var driver;
 
-  //Before
-  test.before(function *() {
-    driver = yield new Builder().forBrowser('chrome').build();
+  test.before(function() {
+    driver = new webdriver.Builder().forBrowser('chrome').build();
+    //driver = new webdriver.Builder().forBrowser('ie').build();
+    //driver = new webdriver.Builder().forBrowser('firefox').build();
   });
 
-  //Test
-  test.it('works with generators', function*() {
-    yield driver.get('http://www.google.com/ncr');
-    yield driver.findElement(By.name('q')).sendKeys('webdriver');
-    yield driver.findElement(By.name('btnG')).click();
-    yield driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+  test.it('should append query to title', function() {
+    driver.get('http://www.google.com');
+    driver.manage().timeouts().implicitlyWait(10000/*ms*/);
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(until.titleIs('webdriver - Поиск в Google'), 10000);
   });
 
-  //After
-  test.after(() => driver.quit());
+  test.after(function() {
+    driver.quit();
+  });
 });
